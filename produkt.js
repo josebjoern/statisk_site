6e22b98f128086686fd30f5465299afcc9a08e0d
Josephine Bjørn Christiansen <jose.bjoern@gmail.com>
2023-02-03 13:25:33 +0100

    js produkt

---
+++ produkt.js
@@ -0,0 +1,21 @@
+const id = 1532;
+const url = `https://kea-alt-del.dk/t7/api/products/1532`;
+const imagePath=`https://kea-alt-del.dk/t7/images/webp/640/1565.webp`;
+
+function hentData(){
+fetch(url)
+.then(res=>res.json())
+.then(visProdukt);
+}
+
+function visProdukt(produkt){
+    console.log(produkt);
+    document.querySelector("#model").textContent = produkt.productdisplayname;
+    document.querySelector("img").src = imagePath;
+    document.querySelector("img").alt = produkt.productdisplayname;
+    document.querySelector(".farve").textContent = produkt.basecolour;
+   
+
+}
+
+hentData();
