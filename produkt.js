b47952c7f10b28986f526dd0bf61d1bb5b411a50
Josephine Bjørn Christiansen <jose.bjoern@gmail.com>
2023-02-06 09:55:47 +0100

    produktliste

--- produkt.js
+++ produkt.js
@@ -1,6 +1,6 @@
 const id = 1532;
 const url = `https://kea-alt-del.dk/t7/api/products/1532`;
-const imagePath=`https://kea-alt-del.dk/t7/images/webp/640/1565.webp`;
+const imagePath=`https://kea-alt-del.dk/t7/images/webp/640/1532.webp`;
 
 function hentData(){
 fetch(url)
@@ -10,10 +10,14 @@ fetch(url)
 
 function visProdukt(produkt){
     console.log(produkt);
-    document.querySelector("#model").textContent = produkt.productdisplayname;
+    document.querySelector(".purchaseBox h4").textContent = produkt.productdisplayname;
     document.querySelector("img").src = imagePath;
     document.querySelector("img").alt = produkt.productdisplayname;
     document.querySelector(".farve").textContent = produkt.basecolour;
+    document.querySelector("#model").textContent = produkt.productdisplayname;
+
+
+
    
 
 }
--- produktliste.js
+++ produktliste.js
@@ -1,4 +1,60 @@
-const filNavn = "https://kea-alt-del.dk/t7/api/products";
+async function hentData(){
+    const response = await fetch("https://kea-alt-del.dk/t7/api/products");
+    const data = await response.json();
+    //console.log(data);
+
+    data.forEach(showProduct);
+}
+
+hentData();
+
+function showProduct(produkt){
+
+const template = document.querySelector("#produktDiscountTemplate").textContent;
+console.log(template);
+
+const copy = template.cloneNode(true);
+copy.querySelector("h3").textContent = produkt.productdisplayname;
+if(product.soldout){
+    copy.querySelector("article").classList.add("soldOut")
+}
+if(product.discount){
+    copy.querySelector("article").classList.add("onSale")
+}
+document.querySelector("main").appendChild(copy);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const filNavn = "https://kea-alt-del.dk/t7/api/products";
 
 function hentData(navn){
     fetch(navn)
@@ -15,10 +71,10 @@ const beholder = document.querySelector("main");
     
     const klon = skabelon.cloneNode(true);
     klon.querySelector(".price").textContent = produkt.price; 
-    klon.querySelector("h2").textContent = produkt.; 
-    klon.querySelector("img").src = hund.billede; 
+    klon.querySelector("h2").textContent = produkt.productdisplayname; 
+    klon.querySelector("img").src = 
     beholder.appendChild(klon);
 });
 }
 
-hentData(filNavn);
+hentData(filNavn);*/
